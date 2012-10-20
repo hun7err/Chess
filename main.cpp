85f70bfedf09ea4e5325bec9f09287a756358f02
hun7err <hun7err@gmail.com>
2012-10-21 01:43:32 +0200

    Just some comments added, nothing important this time

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    Game *game = new Game();
-    game->setWBoardParent(&(w.boardwid));
-    game->setBoard(QColor("#ffffff"), QColor("#303030"));
+    Game *game = new Game();				// 
+    game->setWBoardParent(&(w.boardwid));		// set widget board parent
+    game->setBoard(QColor("#ffffff"), QColor("#303030"));	// setBoard(evenColor, oddColor) - even = even at 8 (or a8), odd respectively to that
     return a.exec();
 }
